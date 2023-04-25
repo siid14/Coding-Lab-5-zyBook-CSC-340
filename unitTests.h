cf0b2fbadc5db5b16cfbcbc65658b72b346614e9
Sidney <sid.14boys@yahoo.fr>
2023-04-24 20:13:28 -0700

    artwork test done

--- unitTests.h
+++ unitTests.h
@@ -24,10 +24,135 @@ namespace NS_UTESTS
     //   . setStyle() and getStyle()    --->bool test_set_getStyle_Artwork();
     //   . setDimension(), getWidth(), getHeight() and getDepth() --->bool test_setDimension_Artwork();
 
-    // class Artist; a partial unit testing.
-    // write one unit test each for the following member functions of the Artist class
-    //  .default constructor and accessors -->bool test_Artist();
-    //  .setID() and getID()--->bool test_set_getID_Artist();
+    // default constructor and accessors --->bool test_Artwork();
+    bool test_Artwork()
+    {
+        // create an Artwork object with default constructor
+        Artwork artwork;
+
+        // check that the default values are set correctly using accessors
+        if (artwork.getID() != -1)
+        {
+            return false;
+        }
+        if (artwork.getArtistID() != -1)
+        {
+            return false;
+        }
+        if (artwork.getType() != ArtType::painting)
+        {
+            return false;
+        }
+        if (artwork.getStyle() != ArtStyle::fineArt)
+        {
+            return false;
+        }
+        if (artwork.getSubject() != ArtSubject::nature)
+        {
+            return false;
+        }
+        if (artwork.getYearMade() != 2023)
+        {
+            return false;
+        }
+        if (artwork.getWidth() != 0.00)
+        {
+            return false;
+        }
+        if (artwork.getHeight() != 0.00)
+        {
+            return false;
+        }
+        if (artwork.getDepth() != 0.00)
+        {
+            return false;
+        }
+        if (artwork.getPrice() != 0.00)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    // setStyle() and getStyle()    --->bool test_set_getStyle_Artwork();
+    bool test_set_getStyle_Artwork()
+    {
+        Artwork artwork;
+
+        // Test setting and getting ArtStyle::fineArt
+        artwork.setStyle(ArtStyle::fineArt);
+        if (artwork.getStyle() != ArtStyle::fineArt)
+        {
+            return false;
+        }
+
+        // Test setting and getting ArtStyle::abstract
+        artwork.setStyle(ArtStyle::abstract);
+        if (artwork.getStyle() != ArtStyle::abstract)
+        {
+            return false;
+        }
+
+        // Test setting and getting ArtStyle::modern
+        artwork.setStyle(ArtStyle::modern);
+        if (artwork.getStyle() != ArtStyle::modern)
+        {
+            return false;
+        }
+
+        // Test setting and getting ArtStyle::popArt
+        artwork.setStyle(ArtStyle::popArt);
+        if (artwork.getStyle() != ArtStyle::popArt)
+        {
+            return false;
+        }
+
+        // Test setting and getting ArtStyle::other
+        artwork.setStyle(ArtStyle::other);
+        if (artwork.getStyle() != ArtStyle::other)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // setDimension(), getWidth(), getHeight() and getDepth() --->bool test_setDimension_Artwork();
+    bool test_setDimension_Artwork()
+    {
+        // create a new artwork
+        Artwork a;
+
+        // set the dimensions of the artwork
+        a.setDimension(10.0, 20.0, 30.0);
+
+        // check that the width, height, and depth have been set correctly
+        if (a.getWidth() != 10.0)
+            return false;
+        if (a.getHeight() != 20.0)
+            return false;
+        if (a.getDepth() != 30.0)
+            return false;
+
+        // set the dimensions of the artwork again
+        a.setDimension(15.0, 25.0, 35.0);
+
+        // check that the width, height, and depth have been set correctly
+        if (a.getWidth() != 15.0)
+            return false;
+        if (a.getHeight() != 25.0)
+            return false;
+        if (a.getDepth() != 35.0)
+            return false;
+
+        // all tests passed
+        return true;
+    }
+
+    //  class Artist; a partial unit testing.
+    //  write one unit test each for the following member functions of the Artist class
+    //   .default constructor and accessors -->bool test_Artist();
+    //   .setID() and getID()--->bool test_set_getID_Artist();
 
     // class Customer; a partial unit testing.
     // write one unit test each for the following member functions of the Customer class
